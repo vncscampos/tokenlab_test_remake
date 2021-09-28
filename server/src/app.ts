6b78715bc52d0f24770554fc293ed1f4fbce95ad
vncscampos <viniolic2018@gmail.com>
2021-09-28 10:50:44 -0300

    Habilitando cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { ApolloServer, ApolloError } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { GraphQLError } from 'graphql';
+import cors from 'cors';
 
 import './database';
 import { resolvers } from './resolvers';
@@ -9,28 +10,30 @@ import ensureAuthenticated from './app/middlewares/ensureAuthenticated';
 
 export async function startApolloServer() {
   const app = express();
+  
+  app.use(cors());
 
   // ApolloServer startup
   const server = new ApolloServer({
     schema: await buildSchema({
       resolvers,
-      authChecker: ensureAuthenticated
+      authChecker: ensureAuthenticated,
     }),
     context: ({ req, res }) => {
       const context = {
         req,
         token: req?.headers?.authorization,
-    };
+      };
 
-    return context;
+      return context;
     },
     formatError: (error: GraphQLError) => {
-      if(error.originalError instanceof ApolloError) {
+      if (error.originalError instanceof ApolloError) {
         return error;
       }
 
       return new GraphQLError(error.message);
-    }
+    },
   });
 
   // Logic to integrate express
